2aa009637f89593d02fbf10cc3857cb73282e55d
xu.hongrui <xu.hongrui@uneed.com>
2020-08-07 18:02:06 +0800

    fix:

---
+++ promise.js
@@ -0,0 +1,32 @@
+function Promise(exe) {
+  let self = this
+  self.status = 'pending'
+  self.vlaue = undefined
+  self.reason = undefined
+  function resolve(value) {
+    if (self.status === 'pending') {
+      self.status = 'resolved'
+      self.value = value
+    }
+  }
+  function reject(reson) {
+    if (self.status == 'pending') {
+      self.status = 'rejected'
+      self.reason = reason
+    }
+  }
+  try {
+    exe(resolve, reject)
+  } catch (error) {
+    reject(error)
+  }
+}
+Promise.prototype.then = function(onFufiled, onRejected) {
+  let self = this
+  if (self.status === 'resolved') {
+    onFufiled(self.value)
+  }
+  if (self.status == 'rejected') {
+    onRejected(self.reason)
+  }
+}
---
+++ viewport.js
@@ -0,0 +1,3 @@
+// <meta name="viewport" content="width=device-width",initial-scale=1.0,maximum-scale=1.0;minimum-scale=1.0,user-scalable=no">
+// viewport 并非只是ios上的独有属性 在安卓和winphone上同样也有viewport 他们要解决的问题是相同的
+// 即无视设备的真实分辨率 直接通过dpi 在物理尺寸和浏览器之间重设分辨率 这个分辨率和设备的分辨率无关
