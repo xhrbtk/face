64ca7ca60742691e0e9400fce9754fca3c474944
xhrbtk <xhtbtk@gmail.com>
2020-08-18 08:59:39 +0800

    fix

--- catchel-control.js
+++ catchel-control.js
@@ -2,21 +2,21 @@
 
 // 如果max-age和Expires同时存在，则被Cache-Control的max-age覆盖。
 
-// cache-control: max-age
+// cache-control: max-age 浏览器事件
 
-// Expires 缓存过期时间 用来指定资源到期的时间 是服务器端的具体的时间点
+// Expires 缓存过期时间 用来指定资源到期的时间 根据的是客户端时间 但是正因为是绝对时间，如果客户端时间被随意更改下，这个机制就失效了
 
 // [Cache-Control]优先级高于[Expires]
 
 // 强缓存 不会向服务器发送请求 直接从缓存中读取资源
 // 协商缓存 强制缓存失效后 浏览器携带缓存标识向服务器发起请求 由服务器根据缓存标识决定是否使用缓存的过程 协商缓存生效 返回304 和 not modified
 
-// 1 Last-Modified和If-Modified-Since
+// 1 Last-Modified和If-Modified-Since 服务器时间
 // 缺点：1、某些服务端不能获取精确的修改时间  2、文件修改时间改了，但文件内容却没有变
 
 // etag 是对该资源的一种唯一标识  只要资源有变化 etag就会重新生成 在下次向服务器发送请求的时候 会将
 // 上一次返回的etag值放在 if-none-match里面 服务器只需要比较客户端传阿里的if-none-match跟自己服务器上该
-//资源的etag是否一致
+//资源的etag是否一致    ----     分布式系统尽量关闭掉ETag(每台机器生成的ETag都会不一样）
 
 // 2.2.3 协商缓存两种方式的对比
 // 优先级上，服务器校验优先考虑Etag
