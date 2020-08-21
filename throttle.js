80dfc3e3ce35822c3f7ce0f57c275459429e7569
xu.hongrui <xu.hongrui@uneed.com>
2020-08-21 10:53:24 +0800

    fix

--- throttle.js
+++ throttle.js
@@ -303,3 +303,53 @@ const newObj = new Proxy(obj, {
         return Reflect.set(target, key, value, receiver)
     },
 })
+
+function diPromiseAll(promises) {
+    return new Promise((resolve, reject) => {
+        // 参数判断
+        if (!Array.isArray(promises)) {
+            throw new TypeError('promises must be an array')
+        }
+        let result = [] // 存放结果
+        let count = 0 // 记录有几个resolved
+        promises.forEach((promise, index) => {
+            promise.then(
+                res => {
+                    result[index] = res
+                    count++
+                    count === promises.length && resolve(result) // 判断是否已经完成
+                },
+                err => {
+                    reject(err)
+                }
+            )
+        })
+    })
+}
+
+let p1 = Promise.resolve(1),
+    p2 = Promise.resolve(2),
+    p3 = Promise.resolve(3)
+
+diPromiseAll([p1, p2, p3]).then(
+    res => {
+        console.log(res, 'res')
+    },
+    err => {
+        console.log(err, 'err')
+    }
+)
+
+// [1, 2, 3]
+
+let p1 = Promise.reject(1),
+    p2 = Promise.resolve(2),
+    p3 = Promise.resolve(3)
+diPromiseAll([p1, p2, p3]).then(
+    res => {
+        console.log(res, 'res')
+    },
+    err => {
+        console.log(err, 'err')
+    }
+)
