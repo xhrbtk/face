bdc6e1785c643ba0a460be284921e6600bb80f36
xu.hongrui <xu.hongrui@uneed.com>
2020-09-03 19:30:52 +0800

    fix

--- 数据结构/tree.js
+++ 数据结构/tree.js
@@ -1,15 +1,20 @@
 // 二叉搜索树
 // 二叉搜索树 只允许你在左侧节点存储比父节点小的的值 在右侧节点存储比父节点大或者等于的值
 function BinarySearchTree() {
-    let Node = function(key) {
+    var Node = function(key) {
         this.key = key
         this.left = null
         this.right = null
+        console.log('this.key', this.key)
     }
     this.insert = function(key) {
-        let newNode = new Node(key)
-        if (root === null) {
+        console.log('我在insert', key)
+        var newNode = new Node(key)
+        console.log('newNode', newNode)
+        console.log('root', root)
+        if (!root) {
             root = newNode
+            console.log('root', root.key)
         } else {
             insertNode(root, newNode)
         }
@@ -33,12 +38,12 @@ function BinarySearchTree() {
 
 var tree = new BinarySearchTree()
 tree.insert(1)
-tree.insert(2)
-tree.insert(0)
+// tree.insert(2)
+// tree.insert(0)
 
-function preOrder(node) {
-    console.log(node)
-    preOrder(node.left)
-    preOrder(node.right)
-}
-preOrder(1)
+// function preOrder(node) {
+//     console.log(node)
+//     preOrder(node.left)
+//     preOrder(node.right)
+// }
+// preOrder(1)
