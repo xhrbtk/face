436fd2c137a04bcb9d8789bdf1855326014b57cb
xhrbtk <xhtbtk@gmail.com>
2021-03-18 08:59:13 +0800

    fix

---
+++ js第四版学习记录/语言基础.js
@@ -0,0 +1,7 @@
+
+// const 声明的限制只适用于它指向的变量的引用 
+// 如果const变量引用的是一个对象 那么修改这个对象内部的属性 并不违反const的限制
+
+// 基本类型： undefined null boolean number string symbol
+// 任何无法表示的负数以 -Infinity  表示 
+// 任何无法表示的正数以Infinity 表示
