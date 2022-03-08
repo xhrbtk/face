838636475749fc0211ef528648a16091016c1beb
xu.hongrui <xu.hongrui@uneed.com>
2022-03-08 12:35:10 +0800

    fix

--- xx.js
+++ xx.js
@@ -44,40 +44,4 @@
 // }
 // xx(arr)
 
-let arr = [
-    {id: 1, name: '部门1', pid: 0},
-    {id: 2, name: '部门2', pid: 1},
-    {id: 3, name: '部门3', pid: 1},
-    {id: 4, name: '部门4', pid: 3},
-    {id: 5, name: '部门5', pid: 4},
-]
-function arrToTree(items){
-    let result = []
-    let itemMap = {}
-    for(let item of items){
-        let id = item.id
-        let pid = item.pid
-        if(!itemMap[id]){
-            itemMap[id] = {
-                children: []
-            }
-        }
-        itemMap[id] = {
-            ...item,
-            children: itemMap[id]['children']
-        }
-        let treeItem = itemMap[id]
-        if(pid === 0){
-            result.push(treeItem)
-        }else{
-            if(!itemMap[pid]){
-                itemMap[pid] = {
-                    children: []
-                }
-            }
-            itemMap[pid].children.push(treeItem)
-        }
-    }
-    console.log(result)
-}
-arrToTree(arr)
+
--- 数组操作.js
+++ 数组操作.js
@@ -13,3 +13,29 @@ Array.prototype.join = function (char) {
 }
 
 // 伪数组与真数组的区别是 伪数组的原型链中没有Array.prototype 而真数组的原型链中有Array.prototype 因此伪数组没有pop和join等属性
+
+// 数组转tree
+let arr = [
+    {id: 1, name: '部门1', pid: 0},
+    {id: 2, name: '部门2', pid: 1},
+    {id: 3, name: '部门3', pid: 1},
+    {id: 4, name: '部门4', pid: 3},
+    {id: 5, name: '部门5', pid: 4},
+]
+function arrToTree(arr){
+    let result = []
+    let map = new Map()
+    for(let item of arr){
+      map.set(item.id, { ...item, children: []})
+    }
+    for(let item of arr){
+        let { id, pid } = { ...item }
+        if(map.get(pid)){
+            map.get(pid).children.push(map.get(id))
+        }else{
+            result.push(map.get(id))
+        }
+    }
+    console.log(result)
+}
+arrToTree(arr)
